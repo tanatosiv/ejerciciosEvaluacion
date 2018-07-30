f8540526a1c528386907872775beb03ee66a0b7a
tanatosiv <tanatosiv@gmail.com>
2018-07-30 16:33:39 -0500

    nav y header

--- replicaAngulario/src/app/app.component.ts
+++ replicaAngulario/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NavComponent} from './nav/nav.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
+
 export class AppComponent {
   title = 'replicaAngulario';
 }
--- replicaAngulario/src/app/app.module.ts
+++ replicaAngulario/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,CommonModule, 
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
