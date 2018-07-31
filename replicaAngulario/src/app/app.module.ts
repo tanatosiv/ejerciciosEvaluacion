a71354f2ee574a67deac63a79e040ba7ea5eb177
tanatosiv <tanatosiv@gmail.com>
2018-07-31 15:33:54 -0500

    secciones

--- replicaAngulario/src/app/app.module.ts
+++ replicaAngulario/src/app/app.module.ts
@@ -6,12 +6,26 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
+import { Section3Component } from './section3/section3.component';
+import { Section4Component } from './section4/section4.component';
+import { Section5Component } from './section5/section5.component';
+import { Section6Component } from './section6/section6.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent,
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    Section4Component,
+    Section5Component,
+    Section6Component
   ],
   imports: [
     BrowserModule,CommonModule, 
